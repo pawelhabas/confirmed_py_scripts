9c4c49ce382b9d362a73b2f9f6da3d14ba0e2e7d
pawelhabas <75744507+pawelhabas@users.noreply.github.com>
2021-02-09 16:07:30 +0100

    Create py_pyqt5.py
    Podstawowe komendy do biblioteki PyQt5

---
+++ py_pyqt5.py
@@ -0,0 +1,33 @@
+from PyQt5.QWidgets import QApplication, QWidget
+import sys
+
+class MyWindow(QWidget):
+
+  def __init__(self):
+    super().__init__()
+    
+    # Ustawienie pozycji i wymiarów (pozycja x,y, wymiary x,y)
+    self.setGemometry(100,100, 800,600) 
+    
+    # Ustawienie tytułu okna
+    self.setWindowTitle("Tytuł okna")
+    
+    # Ustawienie ikony dla okna
+    self.setWindowIcon("myIcon.png")
+    
+    # Ustawienie wymiarów okna na stałe - bez możliwości resize okna
+    self.setFixedHeight(400)
+    self.setFixedWidth(300)
+    
+    # Ustawienie przezroczystości okna
+    self.setWindowOpacity(0.5)
+    
+    # Ustawienie parametrów graficznych okna, np. kolor tła
+    self.setStyleSheet("background-color:green")
+    
+    
+    self.show()
+    
+app = QApplication(sys.argv)
+window = MyWindow()
+sys.exit(app.exec_())
